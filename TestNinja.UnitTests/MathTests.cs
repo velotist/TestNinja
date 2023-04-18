2bd1fc853da2f7a775dc0d84794501d1f60f4f07
Marcus Greiner <marcus.greiner>
2023-04-18 10:50:20 +0200

    Implement Math unit tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var math = new Math();
+            
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_IsFirstArgument_ReturnFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_IsSecondArgument_ReturnSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_AreBothArguments_ReturnSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
