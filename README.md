ab947cb4002cef1fd305fb92c34042b3f8c37be5
Marcus Greiner <marcus.greiner>
2023-04-18 12:18:52 +0200

    Implement string tests - Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # TestNinjaStarter
+
+*Based on*
+[Mosh Hamedani - Unit Testing for C# Developers](https://www.udemy.com/course/unit-testing-csharp/)
+
+## Test Driven Development
+- Write a failing test **first**.
+- **Test** only the **outcome** of a method.
+- Write the simplest code to make the test pass.
+- Refactor your code.
+
+## Takeaways
+When you write your tests after production code,
+- Run your test
+    - if it passes then go in the production code and -><p>
+    - Make a small change in the line that is supposed to make the test pass
+- Create a bug (return a different value or comment out that line)
+    - if your test still passes <p>
+-> that means that your test is not running the right thing
+
+- Do not test
+    - language features
+    - third party code
+
---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public  class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithTheElementStrong()
+        {
+            var formatter = new HtmlFormatter();
+            const string startText = "<strong>";
+            const string text = "This is a test.";
+            const string endText = "</strong>";
+            var expected = $"{startText}{text}{endText}";
+
+            var result = formatter.FormatAsBold(text);
+
+            Assert.That(result, Is.EqualTo(expected).IgnoreCase);
+
+            // More general would be like
+            Assert.That(result, Does.StartWith(startText).IgnoreCase);
+            Assert.That(result, Does.EndWith(endText).IgnoreCase);
+            Assert.That(result, Does.Contain(text).IgnoreCase);
+        }
+    }
+}
