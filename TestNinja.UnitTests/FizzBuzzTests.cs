0b420a51cef93f6fc9fd0a7ce48d1700d1cad7bf
Marcus Greiner <marcus.greiner>
2023-04-18 15:14:45 +0200

    Implement FizzBuzz tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(3,"Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(99, "Fizz")]
+        public void GetOutput_DivisibleOnlyByThree_ReturnFizz(int number, string expected)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(100, "Buzz")]
+        public void GetOutput_DividableOnlyByFive_ReturnBuzz(int number, string expected)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(90, "FizzBuzz")]
+        public void GetOutput_DivisibleOnlyByThreeAndFive_ReturnFizzBuzz(int number, string expected)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetOutput_OfZero_ReturnFizzBuzz()
+        {
+            const int number = 0;
+            const string expected = "FizzBuzz";
+
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(7, "7")]
+        [TestCase(11, "11")]
+        [TestCase(31, "31")]
+        public void GetOutput_NotDivisibleByThreeAndOrFive_ReturnNumber(int number, string expected)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
