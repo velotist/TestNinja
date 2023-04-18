e7571219e9251474e8b39872394616b08bdb565f
Marcus Greiner <marcus.greiner>
2023-04-18 09:04:57 +0200

    Make InstallerHelper immutable

--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -4,7 +4,12 @@ namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
-        private string _setupDestinationFile;
+        private readonly string _setupDestinationFile;
+
+        public InstallerHelper(string setupDestinationFile)
+        {
+            _setupDestinationFile = setupDestinationFile;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
