efe7a6bc1cf2836d9fa243fd1d6a828acaa2869d
Marcus Greiner <marcus.greiner>
2023-04-18 08:56:05 +0200

    Create Reservation unit tests

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -7,7 +7,7 @@ namespace TestNinja.UnitTests
     public class ReservationTests
     {
         [TestMethod]
-        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
         {
             var reservation = new Reservation();
 
@@ -18,13 +18,47 @@ namespace TestNinja.UnitTests
 
             Assert.IsTrue(result);
         }
+
         [TestMethod]
-        public void CanBeCancelledBy_UserIsMadeBy_ReturnsTrue()
+        public void CanBeCancelledBy_UserIsNotAdmin_ReturnFalse()
         {
+            var reservation = new Reservation();
+            // default boolean value is false,
+            // so property IsAdmin of object User is false in this scenario
+            var user = new User();
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsFalse(result);
         }
+
         [TestMethod]
-        public void CanBeCancelledBy_AnotherUser_ReturnsFalse()
+        public void CanBeCancelledBy_SameUser_ReturnTrue()
         {
+            var user = new User();
+            var reservation = new Reservation
+            {
+                MadeBy = user
+            };
+            
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_DifferentUser_ReturnFalse()
+        {
+            var user = new User();
+            var reservation = new Reservation
+            {
+                MadeBy = user
+            };
+            var anotherUser = new User();
+
+            var result = reservation.CanBeCancelledBy(anotherUser);
+
+            Assert.IsFalse(result);
         }
     }
 }
