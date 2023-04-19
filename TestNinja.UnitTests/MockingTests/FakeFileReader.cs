257624cce4306a1d695bf937fd91510680abad1e
Marcus Greiner <marcus.greiner>
2023-04-19 11:06:28 +0200

    Implement VideoService tests with fake class

---
+++ TestNinja.UnitTests/MockingTests/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
--- TestNinja.UnitTests/MockingTests/VideoServiceTests.cs
+++ TestNinja.UnitTests/MockingTests/VideoServiceTests.cs
@@ -1,9 +1,19 @@
 ﻿using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.MockingTests
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
     }
 }
