23bfecb1478761439176abdc54b2fec517b19db5
Marcus Greiner <marcus.greiner>
2023-04-19 13:08:50 +0200

    Test interaction between two objects

---
+++ TestNinja.UnitTests/MockingTests/OrderServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var mockStorage = new Mock<IStorage>();
+            var orderService = new OrderService(mockStorage.Object);
+            var order = new Order();
+            
+            orderService.PlaceOrder(order);
+
+            mockStorage.Verify(s => s.Store(order));
+        }
+    }
+}
