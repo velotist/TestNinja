76581d9a7b89f475bd6e623cd3e0ebf1ef7a6474
Marcus Greiner <marcus.greiner>
2023-04-19 10:57:17 +0200

    Use video service

--- TestNinja/Program.cs
+++ TestNinja/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace TestNinja
+﻿using System;
+using TestNinja.Mocking;
+
+namespace TestNinja
 {
     public class Program
     {
         public static void Main()
         {
+            var service = new VideoService();
+            var title = service.ReadVideoTitle(new FileReader());
 
+            Console.WriteLine(title);
+
+            Console.ReadKey();
         }
     }
 }
