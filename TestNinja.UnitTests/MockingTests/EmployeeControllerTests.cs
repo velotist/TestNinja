df3f3153e2cec341f1f7f7d43a5fff82d65e448a
Marcus Greiner <marcus.greiner>
2023-04-20 19:02:41 +0200

    Implement EmployeeController tests

---
+++ TestNinja.UnitTests/MockingTests/EmployeeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private EmployeeController _controller;
+        private Mock<IEmployeeStorage> _storage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IEmployeeStorage>();
+            _controller = new EmployeeController(_storage.Object);
+        }
+
+        [Test]
+        public void DeleteEmployee_GivenEmployee_ReturnRedirect()
+        {
+            const int employeeId = 1;
+
+            var result = _controller.DeleteEmployee(employeeId);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+
+        [Test]
+        public void DeleteEmployee_GivenEmployee_EmployeeDeletedInStorage()
+        {
+            const int employeeId = 1;
+
+            _controller.DeleteEmployee(employeeId);
+
+            _storage.Verify(storage => storage.DeleteEmployee(employeeId));
+        }
+    }
+}
--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -11,6 +11,9 @@ namespace TestNinja.Mocking
             _employeeStorage = employeeStorage;
         }
 
+        // Test cases:
+        // a) method returns right result
+        // b) ensure that the method deletes the given employee
         public ActionResult DeleteEmployee(int id)
         {
             _employeeStorage.DeleteEmployee(id);
