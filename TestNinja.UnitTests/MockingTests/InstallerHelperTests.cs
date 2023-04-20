d18dfcaf73c7bcf563f80b1814e72e7bfde46a87
Marcus Greiner <marcus.greiner>
2023-04-20 14:33:08 +0200

    Implement InstallerHelper tests

---
+++ TestNinja.UnitTests/MockingTests/InstallerHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private InstallerHelper _installerHelper;
+        private Mock<IFileDownloader> _fileDownloader;
+        private readonly string _installerDestinationFileName = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object, _installerDestinationFileName);
+        }
+
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            const string customerName = "customer";
+            const string installerName = "installer";
+            _fileDownloader.Setup(downloader => downloader
+                    .DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller(customerName, installerName);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadSuccessful_ReturnTrue()
+        {
+            const string customerName = "customer";
+            const string installerName = "installer";
+
+            var result = _installerHelper.DownloadInstaller(customerName, installerName);
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -13,12 +13,16 @@ namespace TestNinja.Mocking
             _setupDestinationFile = setupDestinationFile;
         }
 
+
+        // Test cases:
+        // if download fails because of a WebException, method returns false
+        // if file was downloaded, method returns true
         public bool DownloadInstaller(string customerName, string installerName)
         {
             try
             {
                 _fileDownloader.DownloadFile(
-                    $"http://example.com/{customerName}/{installerName}", 
+                    $"http://example.com/{customerName}/{installerName}",
                     _setupDestinationFile);
 
                 return true;
