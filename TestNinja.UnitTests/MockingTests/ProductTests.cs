037110bb391af62b2cb8a44f27e0d3eae535a0de
Marcus Greiner <marcus.greiner>
2023-04-20 09:52:34 +0200

    Implement Product tests

---
+++ TestNinja.UnitTests/MockingTests/ProductTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_IsGoldCustomer_ReturnPrice()
+        {
+            var product = new Product
+            {
+                ListPrice = 10
+            };
+            var customer = new Customer
+            {
+                IsGold = true
+            };
+
+            var price = product.GetPrice(customer);
+
+            Assert.That(price, Is.EqualTo(7));
+        }
+    }
+}
