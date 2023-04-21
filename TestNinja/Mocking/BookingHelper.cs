53707d1bf8b28dd525dada65f099962ddbe110c8
Marcus Greiner <marcus.greiner>
2023-04-21 08:29:21 +0200

    Refactor BookingHelper

--- TestNinja/Mocking/BookingHelper.cs
+++ TestNinja/Mocking/BookingHelper.cs
@@ -6,17 +6,17 @@ namespace TestNinja.Mocking
 {
     public static class BookingHelper
     {
-        public static string OverlappingBookingsExist(Booking booking)
+        // To use DI via constructor you would have to break signature of the class,
+        // => namely to be non-static
+        // Here is a discrepancy: often DI frameworks do not have the possibility to inject via parameter
+        // In our case we assume that the DI framework has the possibility and therefore
+        // => we use the way to inject via parameter
+        public static string OverlappingBookingsExist(Booking booking, IBookingRepository repository)
         {
             if (booking.Status == "Cancelled")
                 return string.Empty;
 
-            var unitOfWork = new UnitOfWork();
-            var bookings =
-                unitOfWork.Query<Booking>()
-                    .Where(
-                        b => b.Id != booking.Id && b.Status != "Cancelled");
-
+            var bookings = repository.GetActiveBookings(booking.Id);
             var overlappingBooking =
                 bookings.FirstOrDefault(
                     b =>
