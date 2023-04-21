c7b127bbf046d5eaf8c95de2831f8c2048ea02a1
Marcus Greiner <marcus.greiner>
2023-04-21 17:59:43 +0200

    Setup and write first test

---
+++ TestNinja.UnitTests/MockingTests/HousekeeperServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Smtp;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+using IEmailSender = TestNinja.Mocking.IEmailSender;
+
+namespace TestNinja.UnitTests.MockingTests
+{
+    [TestFixture]
+    public class HousekeeperServiceTests
+    {
+        private Mock<IUnitOfWork> _unitOfWork;
+        private Mock<IStatementGenerator> _statementGenerator;
+        private Mock<IEmailSender> _emailSender;
+        private Mock<IExtraMessageBox> _messageBox;
+        private HousekeeperService _service;
+        private readonly DateTime _statementDate = new DateTime(2023, 1, 1);
+        private Housekeeper _housekeeper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _housekeeper = new Housekeeper
+            {
+                Email = "a",
+                FullName = "b",
+                Oid = 1,
+                StatementEmailBody = "c"
+            };
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _statementGenerator = new Mock<IStatementGenerator>();
+            _emailSender = new Mock<IEmailSender>();
+            _messageBox = new Mock<IExtraMessageBox>();
+            
+            _service = new HousekeeperService(
+                _unitOfWork.Object,
+                _statementGenerator.Object,
+                _emailSender.Object,
+                _messageBox.Object);
+
+            _unitOfWork
+                .Setup(unitOfWork =>
+                    unitOfWork.Query<Housekeeper>())
+                .Returns(new List<Housekeeper>
+                    {
+                        _housekeeper
+                    }
+                    .AsQueryable());
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_SendEmails()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _statementGenerator
+                .Verify(sg=>
+                sg.SaveStatement(
+                    _housekeeper.Oid,
+                    _housekeeper.FullName,
+                    _statementDate));
+        }
+    }
+}
