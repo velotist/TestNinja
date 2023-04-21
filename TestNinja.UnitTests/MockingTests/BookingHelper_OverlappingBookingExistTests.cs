495f900f343475a94ea9e8dbc0bc035f4dc76c93
Marcus Greiner <marcus.greiner>
2023-04-21 10:29:12 +0200

    Implement additional tests

--- TestNinja.UnitTests/MockingTests/BookingHelper_OverlappingBookingExistTests.cs
+++ TestNinja.UnitTests/MockingTests/BookingHelper_OverlappingBookingExistTests.cs
@@ -50,6 +50,50 @@ namespace TestNinja.UnitTests.MockingTests
             Assert.That(result, Is.Empty);
         }
 
+        [Test]
+        public void BookingStartsBeforeAndFinishesInMiddleOfExistingBooking__ReturnExistingBookingReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = Before(_existingBooking.ArrivalDate),
+                    DepartureDate = After(_existingBooking.ArrivalDate),
+            },
+                _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
+        [Test]
+        public void BookingStartsBeforeAndFinishesAfterExistingBooking__ReturnExistingBookingReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = Before(_existingBooking.ArrivalDate),
+                    DepartureDate = After(_existingBooking.DepartureDate),
+                },
+                _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
+        [Test]
+        public void BookingIsCancelled__ReturnEmptyString()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = After(_existingBooking.ArrivalDate),
+                    DepartureDate = Before(_existingBooking.DepartureDate),
+                    Reference = "a",
+                    Status = "Cancelled"
+                },
+                _repository.Object);
+
+            Assert.That(result, Is.Empty);
+        }
+
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
